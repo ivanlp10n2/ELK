515b1fb8b18a28fadd6bec33a1bdaf0dd4faf3be
Capitan Empanada <ivanlp10n2@gmail.com>
2020-10-11 00:43:16 -0300

    deleting left overs of testing env variables

--- app/src/main/java/com/empanada/elk/app/LoggingEngineController.java
+++ app/src/main/java/com/empanada/elk/app/LoggingEngineController.java
@@ -3,7 +3,6 @@ package com.empanada.elk.app;
 import com.empanada.elk.logging.LoggingEngine;
 import com.empanada.elk.logging.TimerDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,9 +19,6 @@ public class LoggingEngineController {
 
   private LoggingEngine engine;
 
-  @Value("${spring.application.name}")
-  private String APP_NAME;
-
   @Autowired
   LoggingEngineController (LoggingEngine engine){
     this.engine = engine;
@@ -34,13 +30,13 @@ public class LoggingEngineController {
     TimerDTO timer = TimerDTO.of( milliseconds, message.getMessage());
 
     engine.startNewTimer(timer);
-    return new ResponseEntity("Starting new thread logging in APP: " + APP_NAME, HttpStatus.OK);
+    return new ResponseEntity("Starting new logging thread. ", HttpStatus.OK);
   }
 
   @PostMapping("/stop")
   public ResponseEntity stop(){
     engine.stopAllTimers();
-    return new ResponseEntity("Stopping all threads", HttpStatus.OK );
+    return new ResponseEntity("Stopping all threads.", HttpStatus.OK );
   }
 
 }
